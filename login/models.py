589caed5a325153bf322797543c07645794d4913
Subith O U <ousubith@gmail.com>
2021-12-02 20:36:55 +0530

    Updated code

---
+++ login/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class login_table(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    status = models.PositiveIntegerField(default=0)
+
